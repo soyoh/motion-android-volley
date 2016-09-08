9af9a44a252e6635ba7357440f3e6216b8256bdb
Alejandro Hoyos <soyoh1@gmail.com>
2016-09-08 22:59:41 +0200

    move gradle instalation to gradle dependency

--- lib/motion-android-volley.rb
+++ lib/motion-android-volley.rb
@@ -7,7 +7,12 @@ end
 lib_dir_path = File.dirname(File.expand_path(__FILE__))
 
 Motion::Project::App.setup do |app|
+  require "motion-gradle"
+
   app.files.unshift(Dir.glob(File.join(lib_dir_path, "project/**/*.rb")))
 
-  app.vendor_project jar: File.join(File.dirname(__FILE__), '../vendor/volley-1.0.10.jar')
+  app.gradle do
+    dependency 'com.android.volley:volley:1.0.0'
+  end
+    # app.vendor_project jar: File.join(File.dirname(__FILE__), '../vendor/volley-1.0.10.jar')
 end
--- motion-android-volley.gemspec
+++ motion-android-volley.gemspec
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-VERSION = "0.0.1"
+VERSION = "0.0.3"
 
 Gem::Specification.new do |spec|
   spec.name          = "motion-android-volley"
